31b939d9955e0d8272c0d9b57e2e32717ab90f04
Abhishek Agarwal <42578847+abhiagarwal18@users.noreply.github.com>
2020-06-11 23:42:34 +0530

    date-time combined

--- Incremental Data/sqlPipeline.py
+++ Incremental Data/sqlPipeline.py
@@ -10,13 +10,13 @@ def cleanerSaver(data):
             item_v = item
         temp.append(item_v)
     data.loc[:, 'NO. OF SECURITY (POST) \n'] = temp 
-
+'''
     data['BROADCASTE DATE \n'], data['BROADCASTE TIME \n'] = data['BROADCASTE DATE AND TIME \n'].str.split(' ', 1).str
     data.drop(['BROADCASTE DATE AND TIME \n'], axis = 1, inplace = True) 
     print(data.info())
     
     data['BROADCASTE TIME'] = pd.to_datetime(data['BROADCASTE TIME'],format= '%H:%M' ).dt.time
-    data.index.name = 'index'
+    data.index.name = 'index''''
     data.to_csv('final_database.csv')
 
 ''' testing run
--- historical data/sqlPipeline.py
+++ historical data/sqlPipeline.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+    
 def cleanerSaver(data):
     temp = []
     for item in data['NO. OF SECURITY (POST)']:
@@ -9,10 +9,14 @@ def cleanerSaver(data):
             item_v = item
         temp.append(item_v)
     data.loc[:, 'NO. OF SECURITY (POST)'] = temp 
-
+    data.to_csv('final_data.csv', index = False)
+'''
     data['BROADCASTE DATE'], data['BROADCASTE TIME'] = data['BROADCASTE DATE AND TIME'].str.split(' ', 1).str
     data.drop(['BROADCASTE DATE AND TIME'], axis = 1, inplace = True) 
     print(data.info())
-    data.index.name = 'index'
-    data.to_csv('final_database.csv', index = False)
+    data.index.name = 'index'''
+    
+    
 
+data = pd.read_csv('./final_historical_data.csv')
+cleanerSaver(data)
